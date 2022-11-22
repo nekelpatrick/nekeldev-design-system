4c364206b201135451be7f6750397792c463e73f
Patrick Nekel <nekelpatrick@gmail.com>
2022-11-21 22:16:55 -0300

    :package: create tokens package

---
+++ packages/tokens/src/colors.ts
@@ -0,0 +1,18 @@
+export const colors = {
+  white: '#FFF',
+  black: '#000',
+
+  gray100: '#E1E1E6',
+  gray200: '#A9A9B2',
+  gray400: '#7C7C8A',
+  gray500: '#505059',
+  gray600: '#323238',
+  gray700: '#29292E',
+  gray800: '#202024',
+  gray900: '#121214',
+
+  brand300: '#623CEA',
+  brand500: '#4727BA',
+  ignite700: '#3111BA',
+  ignite900: '#1227BA',
+}
