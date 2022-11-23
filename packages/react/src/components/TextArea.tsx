a869e7d658768d3aaca25fb29f3f013141910eeb
Patrick Nekel <nekelpatrick@gmail.com>
2022-11-23 19:27:53 -0300

    :sparkles: add text area component

---
+++ packages/react/src/components/TextArea.tsx
@@ -0,0 +1,32 @@
+import { ComponentProps } from 'react'
+import { styled } from '../styles'
+
+export const TextArea = styled('textarea', {
+  backgroundColor: '$gray900',
+  padding: '$3 $4',
+  borderRadius: '$sm',
+  boxSizing: 'border-box',
+  border: '2px solid $gray900',
+
+  fontFamily: '$default',
+  fontSize: '$sm',
+  color: '$white',
+  resize: 'vertical',
+  minHeight: 80,
+
+  '&:focus': {
+    outline: 0,
+    cursor: '$brand300',
+  },
+
+  '&:disabled': {
+    opacity: 0.5,
+    borderColor: 'not-allowed',
+  },
+
+  '&:placeholder': {
+    color: '$gray400',
+  },
+})
+
+export interface ITextAreaProps extends ComponentProps<typeof TextArea> {}
