c506aaa8befe2b28be967bfdd8b6dd3d2c8c7f83
Patrick Nekel <nekelpatrick@gmail.com>
2022-11-23 16:03:03 -0300

    :memo: add colors tokens to the documentation.

---
+++ packages/docs/src/components/ColorsGrid.tsx
@@ -0,0 +1,23 @@
+import { colors } from '@nekeldev/tokens'
+import { getContrast } from 'polished'
+
+export const ColorsGrid = () => {
+  return Object.entries(colors).map(([key, color]) => {
+    return (
+      <div key={key} style={{ backgroundColor: color, padding: '2rem' }}>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            fontFamily: 'monospace',
+            // Using polished to get the background-color and then apply a contrast color to the font (that's why I love React lol)
+            color: getContrast(color, '#FFF') < 3.5 ? '#000' : '#FFF',
+          }}
+        >
+          <strong>${key}</strong>
+          <span>{color}</span>
+        </div>
+      </div>
+    )
+  })
+}
